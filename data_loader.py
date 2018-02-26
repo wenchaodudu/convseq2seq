346fa0f96e1b8265d4924143336052d223362bdf
Wenchao Du <amadeus0@br018.pvt.bridges.psc.edu>
2018-02-25 20:15:13 -0500

    preprocessing and models

---
+++ data_loader.py
@@ -0,0 +1,92 @@
+import json
+import torch
+import torch.utils.data as data
+
+
+class Dataset(data.Dataset):
+    def __init__(self, src_path, trg_path, dictionary, max_ctxt_len=160):
+        """Reads source and target sequences from txt files."""
+        self.src_seqs = self.preprocess(open(src_path).readlines(), dictionary)
+        self.trg_seqs = self.preprocess(open(trg_path).readlines(), dictionary)
+        self.dictionary = dictionary
+        self.max_ctxt_len = max_ctxt_len
+
+    def __getitem__(self, index):
+        """Returns one data pair (source and target)."""
+        src_seq = self.src_seqs[index]
+        trg_seq = self.trg_seqs[index]
+        return src_seq, trg_seq
+
+    def preprocess(self, sequence, dictionary):
+        """Converts words to ids."""
+        tokens = sequence.strip().split()
+        sequence = []
+        sequence.extend([dictionary[token] for token in tokens])
+        sequence = torch.Tensor(sequence)
+        return sequence
+
+
+def collate_fn(data):
+    """Creates mini-batch tensors from the list of tuples (src_seq, trg_seq).
+
+    We should build a custom collate_fn rather than using default collate_fn,
+    because merging sequences (including padding) is not supported in default.
+    Seqeuences are padded to the maximum length of mini-batch sequences (dynamic padding).
+
+    Args:
+        data: list of tuple (src_seq, trg_seq).
+            - src_seq: torch tensor of shape (?); variable length.
+            - trg_seq: torch tensor of shape (?); variable length.
+
+    Returns:
+        src_seqs: torch tensor of shape (batch_size, padded_length).
+        src_lengths: list of length (batch_size); valid length for each padded source sequence.
+        trg_seqs: torch tensor of shape (batch_size, padded_length).
+        trg_lengths: list of length (batch_size); valid length for each padded target sequence.
+    """
+    def merge(sequences):
+        lengths = [len(seq) for seq in sequences]
+        padded_seqs = torch.zeros(len(sequences), max(lengths)).long()
+        for i, seq in enumerate(sequences):
+            end = lengths[i]
+            padded_seqs[i, :end] = seq[:end]
+        return padded_seqs, lengths
+
+    # sort a list by sequence length (descending order) to use pack_padded_sequence
+    data.sort(key=lambda x: len(x[0]), reverse=True)
+
+    # seperate source and target sequences
+    src_seqs, trg_seqs = zip(*data)
+
+    # merge sequences (from tuple of 1D tensor to 2D tensor)
+    src_seqs, src_lengths = merge(src_seqs)
+    trg_seqs, trg_lengths = merge(trg_seqs)
+
+    return src_seqs, src_lengths, trg_seqs, trg_lengths
+
+
+def get_loader(src_path, trg_path, dictionary, batch_size=200):
+    """Returns data loader for custom dataset.
+
+    Args:
+        src_path: txt file path for source domain.
+        trg_path: txt file path for target domain.
+        src_word2id: word-to-id dictionary (source domain).
+        trg_word2id: word-to-id dictionary (target domain).
+        batch_size: mini-batch size.
+
+    Returns:
+        data_loader: data loader for custom dataset.
+    """
+    # build a custom dataset
+    dataset = Dataset(src_path, trg_path, dictionary)
+
+    # data loader for custome dataset
+    # this will return (src_seqs, src_lengths, trg_seqs, trg_lengths) for each iteration
+    # please see collate_fn for details
+    data_loader = torch.utils.data.DataLoader(dataset=dataset,
+                                              batch_size=batch_size,
+                                              shuffle=True,
+                                              collate_fn=collate_fn)
+
+    return data_loader
